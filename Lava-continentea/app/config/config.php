8288b6f7f325567fe79ebc0ef3ad6b249734974c
Jared <jaredphilippsabaren@gmail.com>
2023-01-19 23:24:27 +0800

    okay na
    Mali lang yung select name hahah naka lagay sa controller niyo supplier_name and ang nakalagay sa form niyo ay name kaya hinahanap niya san galing yung supplier_name

--- Lava-continentea/app/config/config.php
+++ Lava-continentea/app/config/config.php
@@ -80,7 +80,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'http://localhost/Lava-continentea/';
+$config['base_url'] 				= 'http://localhost/continentea-sales-and-inventory/Lava-continentea/';
 
 /*
 |--------------------------------------------------------------------------
